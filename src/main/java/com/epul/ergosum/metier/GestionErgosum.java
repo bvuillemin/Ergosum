659be84801eeea9aa54a29600c924c3b18949999
dorianmein <dorian.mein@etu.univ-lyon1.fr>
2015-03-23 11:55:16 +0100

    ajout methode (pas toutes correctes)

--- src/main/java/com/epul/ergosum/metier/GestionErgosum.java
+++ src/main/java/com/epul/ergosum/metier/GestionErgosum.java
@@ -4,12 +4,42 @@ import com.epul.ergosum.meserreurs.MonException;
 import com.epul.ergosum.persistance.DialogueBd;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by benoitvuillemin on 23/03/2015.
  */
 public class GestionErgosum {
+
+    // par défault qui liste tous les jouets sans critères
+    public List<Jouet> listerTousLesJouets() throws MonException {
+        List<Object> rs;
+        List<Jouet> mesJouets = new ArrayList<Jouet>();
+        int index = 0;
+        String mysql = "";
+
+        mysql = "SELECT * FROM jouet" +
+                " ORDER BY id ASC";
+
+        rs = DialogueBd.lecture(mysql);
+
+        while (index < rs.size()) {
+            // On cr�e un stage
+            Jouet unJ = new Jouet();
+            // il faut redecouper la liste pour retrouver les lignes
+            unJ.setNumero(rs.get(index + 0).toString());
+            unJ.setCategorie(new Categorie(rs.get(index + 1).toString()));
+            unJ.setTrancheage(new Trancheage(rs.get(index + 2).toString()));
+            unJ.setLibelle(rs.get(index + 3).toString());
+            // On incr�mente tous les 4 champs
+            index = index + 4;
+            mesJouets.add(unJ);
+        }
+
+        return mesJouets;
+
+    }
 
     public List<Jouet> listerTousLesJouets(int categorieCode, int trancheCode) throws MonException {
         List<Object> rs;
@@ -140,4 +170,139 @@ public class GestionErgosum {
 
         return monJouet;
     }
+
+    public Categorie rechercherCategorie(String codeCategorie) throws MonException {
+        List<Object> rs;
+
+        String mysql = "";
+
+        mysql = "SELECT * FROM categorie" +
+                "WHERE CODECATEG =" + codeCategorie +
+                " ORDER BY id ASC";
+
+        rs = DialogueBd.lecture(mysql);
+
+        Categorie maCategorie = new Categorie();
+        maCategorie.setCodecateg(rs.get(0).toString());
+        maCategorie.setLibcateg(rs.get(1).toString());
+
+
+        return maCategorie;
+    }
+
+    public Trancheage rechercherTrancheage(String codetranche) throws MonException {
+        List<Object> rs;
+
+        String mysql = "";
+
+        mysql = "SELECT * FROM trancheage" +
+                "WHERE CODETRANCHE =" + codetranche +
+                " ORDER BY id ASC";
+
+        rs = DialogueBd.lecture(mysql);
+
+        Trancheage maTranche = new Trancheage();
+        maTranche.setCodetranche(rs.get(0).toString());
+        maTranche.setAgemin(Integer.valueOf(rs.get(1).toString()));
+        maTranche.setAgemax(Integer.valueOf(rs.get(2).toString()));
+
+        return maTranche;
+    }
+
+    public Catalogue rechercherCatalogue(String codecatalogue) throws MonException {
+        List<Object> rs;
+
+        String mysql = "";
+
+        mysql = "SELECT * FROM catalogue" +
+                "WHERE ANNEE =" + codecatalogue +
+                " ORDER BY id ASC";
+
+        rs = DialogueBd.lecture(mysql);
+
+        Catalogue monCatalogue = new Catalogue();
+        monCatalogue.setAnnee(Integer.valueOf(rs.get(0).toString()));
+        monCatalogue.setQuantiteDistribuee(Integer.valueOf(rs.get(1).toString()));
+
+        return monCatalogue;
+    }
+
+    public void modifier(Jouet unJouet) throws MonException {
+
+        String mysql = "";
+
+        mysql = "UPDATE jouet " +
+                "SET CODECATEG = " + unJouet.getCategorie() +
+                "SET CODETRANCHE = " + unJouet.getTrancheage() +
+                "SET LIBELLE = " + unJouet.getLibelle() +
+                "WHERE NUMERO =" + unJouet.getNumero();
+        DialogueBd.insertionBD(mysql);
+    }
+
+    public void ajouter(Jouet unJouet) throws MonException {
+
+        String mysql = "";
+
+        mysql = "INSERT INTO jouet (NUMERO, CODECATEG, CODETRANCHE, LIBELLE) " +
+                " VALUES ( \'" + unJouet.getNumero() + "\', \'" + unJouet.getCategorie() + "\', " +
+                "\' " + unJouet.getTrancheage() + "\', " + "\' " + unJouet.getLibelle() + " )";
+        DialogueBd.insertionBD(mysql);
+    }
+
+    public void modifierCatalogue(Catalogue leCatalogue) throws MonException {
+
+        String mysql = "";
+
+        mysql = "UPDATE catalogue " +
+                "SET quantiteDistribuee = " + leCatalogue.getQuantiteDistribuee() +
+                "WHERE ANNEE =" + leCatalogue.getAnnee();
+        DialogueBd.insertionBD(mysql);
+
+    }
+
+    public void effacer(String[] ids) throws MonException {
+
+        int i = 0;
+        while (i < ids.length) {
+            String mysql = "";
+            mysql = "DELETE from jouet where id =" + ids[i] +"" ;
+            DialogueBd.insertionBD(mysql);
+        }
+
+
+    }
+// La requete est fausse
+    public int listerCatalogueQuantites(int annee, int nb) throws MonException {
+        List<Object> rs;
+        int index = 0;
+        int anneefin = annee + nb;
+        int quantite = 0;
+
+        String mysql = "";
+        mysql = "SELECT * FROM catalogue"; // il faut aller chercher la quantité cf image
+        rs = DialogueBd.lecture(mysql);
+
+        while (index < rs.size()) {
+            // On cr�e un stage
+            Catalogue unC = new Catalogue();
+            // il faut redecouper la liste pour retrouver les lignes
+            unC.setAnnee(Integer.valueOf(rs.get(index + 0).toString()));
+            unC.setQuantiteDistribuee(Integer.valueOf(rs.get(index + 1).toString()));
+
+            if(unC.getAnnee()>=annee && unC.getAnnee()<=anneefin){
+                quantite = quantite + unC.getQuantiteDistribuee();
+            }
+            // On incr�mente tous les 2 champs
+            index = index + 2;
+        }
+
+        return quantite;
+    }
+
+    //a faire
+    public HashMap<Categorie, Integer> rechercherDictionnaire(String annee){
+
+        return null;
+    }
+
 }
